0f7e4c1a43f8250613bf13a500d23659c1a5eb31
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-05-22 00:26:33 +0530

    done problem#5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,16 @@
-# Functions
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+score = input("Enter Score: ")
+s=float(score)
+if(s<=1 and s>=0):
+    if(s>=0.9):
+        print('A')
+    elif(s>=0.8):
+        print('B')
+    elif(s>=0.7):
+        print('C')
+    elif(s>=0.6):
+        print('D')
+    elif(s<0.6):
+        print('F')
+else:
+    print('please enter a value between 0.0 to 1.0')
