732211841bf4b539d2cfa5849a3ce09571b0131c
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-05-29 23:17:18 +0530

    29_5

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,17 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        n=int(num)
+        if largest is None or n>largest:
+		    largest=n
+        if smallest is None or n<smallest:
+			smallest= n
+	except:
+        print("Invalid input")
+print("Maximum is",largest)
+print("Minimum is",smallest)
+            
