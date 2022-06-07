4d101b3712085b2761e089f81536b54c02eb0252
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-06-07 23:02:31 +0530

    7_6

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,3 @@
-# Files
+#Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
 
-filename = "dataset/mbox-short.txt"
+text= "X-DSPAM-Confidence:    0.8475"
