dbaab532a14e9dfc6cb317fd394a273d9505bd43
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-06-10 09:41:41 +0530

    10_6

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,14 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+y=0
+cnt=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    f=line.find("0.")
+    n=float(line[f:])
+    y=y+n
+    cnt=cnt+1
+avg=y/cnt    
+print('Average spam confidence:',avg)
