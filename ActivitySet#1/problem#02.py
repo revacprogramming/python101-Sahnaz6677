cbc5b97028db359dceb40c8648523f900396c29b
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-06-17 05:32:09 +0530

    17-6-22

--- ActivitySet#1/problem#02.py
+++ ActivitySet#1/problem#02.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+a=int(input("Enter first value"))
+b=int(input("Enter second value"))
+print("Before swapping, value of a=", a)
+print("Before swapping, value of b=", b)
+a,b=b,a
+print("After swapping, value of a=", a)
+print("After swapping, value of b=", b)
