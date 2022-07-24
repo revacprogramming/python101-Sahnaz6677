a5e0aeed95a5d7401cc8257983c591e2be32e2bd
Sahnaz6677 <p.sahnaz18@gmail.com>
2022-07-24 20:26:37 +0530

    24-7

--- ActivitySet#1/problem#02.py
+++ ActivitySet#1/problem#02.py
@@ -1,3 +1,11 @@
 # Write a program that uses a print statement to say 'hello world'
 
-print('helllooo')
+print('hellooo')
+
+str1=str(input("Enter the string"))
+def slicing():
+	m=int(input("Enter the starting index value or index1"))
+	n=int(input("Enter the ending index value or index2"))
+	substr=str1[m:n]
+	print("String extracted between index1 and index2 is",substr)
+slicing()
